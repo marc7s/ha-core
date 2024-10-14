b5057ff0b7b0a28572afe9f654a16090289e5a8e
Jakob Windt <jakob.windt@gmail.com>
2024-10-14 09:48:53 +0000

    Reduce cognitive complexity in flash_briefings.py

--- homeassistant/components/alexa/flash_briefings.py
+++ homeassistant/components/alexa/flash_briefings.py
@@ -53,47 +53,33 @@ class AlexaFlashBriefingView(http.HomeAssistantView):
         super().__init__()
         self.flash_briefings = flash_briefings
 
+    def get_rendered_value(self, item: dict, key: str) -> str | None:
+        """Help function."""
+        value = item.get(key)
+        if value is not None:
+            if isinstance(value, template.Template):
+                return str(value.async_render(parse_result=False))
+            if isinstance(value, str):
+                return value
+        return None
+
     def get_briefing(self, briefing_id: str) -> list:
         """Initialize Alexa view."""
         briefing = []
 
         for item in self.flash_briefings.get(briefing_id, []):
             output = {}
-            if item.get(CONF_TITLE) is not None:
-                if isinstance(item.get(CONF_TITLE), template.Template):
-                    output[ATTR_TITLE_TEXT] = item[CONF_TITLE].async_render(
-                        parse_result=False
-                    )
-                else:
-                    output[ATTR_TITLE_TEXT] = item.get(CONF_TITLE)
 
-            if item.get(CONF_TEXT) is not None:
-                if isinstance(item.get(CONF_TEXT), template.Template):
-                    output[ATTR_MAIN_TEXT] = item[CONF_TEXT].async_render(
-                        parse_result=False
-                    )
-                else:
-                    output[ATTR_MAIN_TEXT] = item.get(CONF_TEXT)
+            output[ATTR_TITLE_TEXT] = self.get_rendered_value(item, CONF_TITLE)
+            output[ATTR_MAIN_TEXT] = self.get_rendered_value(item, CONF_TEXT)
+            output[ATTR_STREAM_URL] = self.get_rendered_value(item, CONF_AUDIO)
+            output[ATTR_REDIRECTION_URL] = self.get_rendered_value(
+                item, CONF_DISPLAY_URL
+            )
 
             if (uid := item.get(CONF_UID)) is None:
                 uid = str(uuid.uuid4())
             output[ATTR_UID] = uid
-
-            if item.get(CONF_AUDIO) is not None:
-                if isinstance(item.get(CONF_AUDIO), template.Template):
-                    output[ATTR_STREAM_URL] = item[CONF_AUDIO].async_render(
-                        parse_result=False
-                    )
-                else:
-                    output[ATTR_STREAM_URL] = item.get(CONF_AUDIO)
-
-            if item.get(CONF_DISPLAY_URL) is not None:
-                if isinstance(item.get(CONF_DISPLAY_URL), template.Template):
-                    output[ATTR_REDIRECTION_URL] = item[CONF_DISPLAY_URL].async_render(
-                        parse_result=False
-                    )
-                else:
-                    output[ATTR_REDIRECTION_URL] = item.get(CONF_DISPLAY_URL)
 
             output[ATTR_UPDATE_DATE] = dt_util.utcnow().strftime(DATE_FORMAT)
 
